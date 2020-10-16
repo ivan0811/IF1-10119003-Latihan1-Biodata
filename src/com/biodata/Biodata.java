8a28107e2ea9710b93d841c39a1e31d54bb22e43
ivan0811 <ivanfaathirza@gmail.com>
2020-10-16 17:57:02 +0700

    initial commit

---
+++ src/com/biodata/Biodata.java
@@ -0,0 +1,22 @@
+package com.biodata;
+
+/*
+	NAMA = Ivan Faathirza
+	KELAS = IF1
+	NIM = 10119003
+	Deskripsi program = program ini untuk menampilkan biodata ke layar
+ */
+public class Biodata {
+
+    public static void main(String[] args) {
+	    System.out.println("BIODATA ROCKSTAR" + "\n" + "------------------");
+	    System.out.println("Nama\t\t\t: Ivan Faathirza");
+	    System.out.println("Alamat\t\t\t: Jl. Teratai No 8");
+	    System.out.println("Tempat Lahir\t: Tasikmalaya");
+	    System.out.println("Tanggal Lahir\t: 08 November 2001");
+	    System.out.println("------------------");
+	    System.out.println("Hobi\t\t\t: Nonton Anime, Ngoding, Bermain Game");
+	    System.out.println("Makes\t\t\t: Mie");
+	    System.out.println("Mikes\t\t\t: Kopi Manis");
+    }
+}
